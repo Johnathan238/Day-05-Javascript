f07663a78cc28afcbddbfa9b929110b5406e59d3
Johnathan238 <60320817+Johnathan238@users.noreply.github.com>
2022-10-27 12:11:24 -0400

    using filter method to retrive strings with '0'

--- level1.js
+++ level1.js
@@ -45,7 +45,7 @@
 
 //6.
 const itCompanies = [
-  "Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle" , "Amazon"
+  "facebook", "google", "microsoft", "apple", "IBM", "oracle" , "amazon"
 ]
 
 //7.
@@ -58,5 +58,23 @@ const itCompanies = [
 // console.log(itCompanies[0], itCompanies[4], itCompanies[6])
 
 //10,
-console.log(itCompanies.join())
+// console.log(itCompanies.join())
+
+//11.
+// let namescap = itCompanies.map((string)=>{
+//   return string[0].toUpperCase() + string.slice(1)
+// })
+// console.log(namescap)
+
+
+//12.
+// console.log(`${namescap} are big IT Companies `)
+
+//13.
+// let index = itCompanies.indexOf("amazon")
+// index === -1 ? console.log(`This company dose not exist`) : console.log(`This company does exist`)
+
+//14.
+const results = itCompanies.filter(itCompanies => itCompanies.includes('o'))
+console.log(results)
 
