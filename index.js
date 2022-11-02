bd32634564fc1a84405851745c743c4d80754029
Johnathan238 <60320817+Johnathan238@users.noreply.github.com>
2022-11-02 18:03:15 -0400

    1 file modified

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ import './level2.js'
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>Day-05-Javascript</h1>`;
+appDiv.innerHTML = `<h1>Javascript</h1>`;
